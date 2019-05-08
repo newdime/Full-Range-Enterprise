947effa1d7faed5130c747fc9a3faebb7cf5f70d
DESKTOP-5DLP4PT\donk box <asylumnsw@protonmail.com>
2019-05-09 08:50:05 +1000

    change report card scripts to standalone

---
+++ report-card-scripts/Code.js
@@ -0,0 +1,18 @@
+function onOpen() {
+  SpreadsheetApp.getUi() // Or DocumentApp or SlidesApp or FormApp.
+  .createMenu('Custom Menu')
+  .addItem('Show sidebar', 'showSidebar')
+  .addToUi();
+
+  //runFile();
+  createHeaders();
+  trig();
+}
+
+function showSidebar() {
+  var html = HtmlService.createHtmlOutputFromFile('Page')
+  .setTitle('Control Panel')
+  .setWidth(300);
+  SpreadsheetApp.getUi() // Or DocumentApp or SlidesApp or FormApp.
+  .showSidebar(html);
+}
