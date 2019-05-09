bb9457e6348588f8a1b971b47f95bb44386bac11
DESKTOP-5DLP4PT\donk box <asylumnsw@protonmail.com>
2019-05-09 11:52:23 +1000

    re-org date

--- date/name.js
+++ date/name.js
@@ -1,14 +1,14 @@
 function name(reportID) {
-  
+
   var reportID = reportID;
-  
+
   var ss = SpreadsheetApp.openById(reportID);
-  
+
   // get the previous week date
   var token = 2;
   var previousWeek = date(token, 0);
   Logger.log(previousWeek);
-  
+
   // change the name of the previous week
   if (ss.getName() == previousWeek)
   {
@@ -17,23 +17,23 @@ function name(reportID) {
     var sheetNames =  date(token, 1);
     Logger.log(name);
     Logger.log(sheetNames);
-    
+
     DriveApp.getFileById(reportID).setName(name);
-    
+
     var sheets = ss.getSheets();
     for (var i in sheets)
     {
       sheets[i].setName(sheetNames[i]);
-    }   
+    }
   }
-  
+
   // get the current date
   if (ss.getName() == 'reportTemplateNear')
   {
     var token = 3;
     var currentWeek = date(token, 0);
     Logger.log(currentWeek);
-    
+
     // change the name of the previous week
     if (ss.getName() == currentWeek)
     {
@@ -42,24 +42,24 @@ function name(reportID) {
       var sheetNames =  date(token, 1);
       Logger.log(name);
       Logger.log(sheetNames);
-      
+
       DriveApp.getFileById(reportID).setName(name);
-      
+
       var sheets = ss.getSheets();
       for (var i in sheets)
       {
         sheets[i].setName(sheetNames[i]);
-      }   
+      }
     }
   }
-  
+
   // get the forward date
   if (ss.getName() == 'reportTemplateFar')
   {
     var token = 1;
     var farWeek = date(token, 0);
     Logger.log(farWeek);
-    
+
     // change the name of the previous week
     if (ss.getName() == farWeek)
     {
@@ -68,17 +68,17 @@ function name(reportID) {
       var sheetNames =  date(token, 1);
       Logger.log(name);
       Logger.log(sheetNames);
-      
+
       DriveApp.getFileById(reportID).setName(name);
-      
+
       var sheets = ss.getSheets();
       for (var i in sheets)
       {
         sheets[i].setName(sheetNames[i]);
-      }   
+      }
     }
   }
-  
+
   // return 0 for complete
-  return 
+  return
 }
---
+++ date/vars.js
@@ -0,0 +1,10 @@
+function vars() {
+  var staffFoldersId = ["1-BaaZ1ZabGYFDdQY3_lga29Vh9k6Jjuh", "1DdHEbcmKyaCdWKF4gTFVa5YxZCYvBxI2", "1oHs8YgTiPlUSGaGvTEx5jMYsFYMkrEMM",
+                        "1P6N9Yel8QQPsXc71J3fuucTE4EsuHupW", "1Kqywy1WC92YCFD7pOJBZHJ_7WSZ04Ga3", "17qnCBk-WiQsraBPW_Esnga8cqKTAFXBu",
+                        "1J6W4wY0-GWDggHJVF1tiwvX1ojNJmMYZ", "1K_vSl7HTe2JNMcWDzNpoTKq2Jxaj5g8T"];
+
+  var staffFolderArchive = ["1zDXNWy2GERhE7R7xAYvl5cS6LTrBu3-z", "1Yp06lwPM3v8hZ861YsLtuCWeWE5Kccgv", "1e1KJBhschXPB7ADrA6umQT_acx3WgZxy",
+                            "1LexvI3ExOR3u8qRD_v_qj-QZGHruKPF7","1VeiGbTO_v4cz39w1DexWnqHtI_DAnAsj", "1ciCbIGKThFpPXURsgfJvaAbVg870l8Zm",
+                            "1fROxe1CsXOWebcc5gqAYdOl1zau-dQYJ", "1nEpdY_pr9w-qLknsJYE6zhuQkIXmwmdn"];
+
+}
