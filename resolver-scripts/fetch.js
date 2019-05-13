6c5013f0324e4845407c78b9f2662a75f035bfed
DESKTOP-5DLP4PT\donk box <asylumnsw@protonmail.com>
2019-05-14 00:33:32 +1000

    resolver fetch

--- resolver-scripts/fetch.js
+++ resolver-scripts/fetch.js
@@ -1,7 +1,7 @@
 function fetch(staff){
-
+  
   var staff = staff;
-
+  
   // get the active resolver
   var resolver = SpreadsheetApp.getActiveSpreadsheet();
   var id = resolver.getId();
@@ -11,13 +11,13 @@ function fetch(staff){
   var namedRange = resolver.getRangeByName('data');
   var domain = staff + '@fullrange.com.au';
   Logger.log(domain);
-
+  
   // get the correct database entries
   var report = SpreadsheetApp.openById(match(2, 0, name));
   var schedule =  SpreadsheetApp.openById(match(0, 0, name));
   Logger.log('report = ' + report.getName());
   Logger.log('schedule = ' + schedule.getName());
-
+  
   // get the correct report sheet
   var reportSheet = report.getSheets();
   Logger.log('getting report data');
@@ -29,7 +29,7 @@ function fetch(staff){
       break;
     }
   }
-
+  
   // get the correct schedule sheet
   var schedSheet = schedule.getSheets();
   Logger.log('getting schedule data');
@@ -41,31 +41,50 @@ function fetch(staff){
       break;
     }
   }
-
-  // capture report data into array
-  var index = 1;
-  while(index <= 7*7)
-  {
-    if (!(index > 7))
-    {
-    }
-    var data1 = reportSheet[i].getRange(index, 1, 7, 12).getValues();
-    
-    //Logger.log(data1);
-    //Logger.log(data2);
-    
-    
-    index = index + 7;
-  }
-    Logger.log(data1[0]);
-    //data1[0].splice(8,3);
-  data1[0].splice(1,0, data1[0][7], data1[0][6], data1[0][5], data1[0][3], data1[0][4], data1[0][1], data1[0][2], data1[0][11]);
-  data1[0].splice(8,data1[0].length - 8 - 1);
-    Logger.log(data1[0]);
   
-  // capture schedule data into arraygetActiveSheet()  // capture schedule data into array
- 
-  // paste report data into resolver sheet
-
-  // paste schedule data into resolver sheet
+  // capture report data into array
+  var data1 = reportSheet[i].getRange(1, 1, 7*7, 12).getValues();
+  //  Logger.log(data1[0]);
+  
+  // capture schedule data into array
+  var data2 = schedSheet[n].getRange(1, 2, 7*7, 1).getValues();
+  //  Logger.log(data2[0]);
+  
+  Logger.log('length = ' + data1.length);
+  
+  // splice the two data sets together
+  var t = 0;
+  while (t < data1.length)
+  {
+    //data1[0].splice(8,3);
+    data1[t].splice(1,0, data1[t][7], data1[t][6], data1[t][5], data1[t][3], data1[t][4], data1[t][1], data1[t][2], data1[t][11]);
+    data1[t].splice(8,data1[t].length - 8 - 1, data2[t][0]);
+    //  Logger.log(data1[t]);
+    t++
+  }
+  
+  // paste report data into sheet
+  //Logger.log(resolver.getRangeByName(staff).getValues());
+  var staffRange = resolver.getRangeByName(staff);
+  var index = staffRange.getRowIndex();
+  var col = staffRange.getColumn();
+  var divider = staffRange.getNumColumns()/staffRange.getNumRows();
+  
+  var l = data1.length;
+  var colInc = 0;
+  var past = ([]);
+  while (!(l < 0))
+  {
+    //Logger.log(data1.splice(l - 7, 0));
+    Logger.log(data1.splice(l, 0));
+    resolver.getRange(index, col + colInc, staffRange.getNumRows(), divider).setValues(paste); 
+    colInc = colInc + divider;
+    
+    l = l - 7; 
+  }
+  
+  Logger.log(staffRange.getNumRows());
+  Logger.log(staffRange.getNumColumns());
+  Logger.log(staffRange);
+  
 }
